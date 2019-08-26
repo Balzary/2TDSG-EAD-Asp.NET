c56d6aaaac5857f254e8571ed6c7aaf3a738d0c8
Logon Aluno <logonaluno@local.com>
2019-08-26 11:36:45 -0300

    Aula 05 Tag Helpers

---
+++ 05-Fiap.Web.AspNet/05-Fiap.Web.AspNet/Controllers/CadastroController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _05_Fiap.Web.AspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _05_Fiap.Web.AspNet.Controllers
+{
+    public class CadastroController : Controller
+    {
+
+        private static IList<Zoo> _lista = new List<Zoo>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Listar()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Zoo zoo)
+        {
+            _lista.Add(zoo);
+            TempData["msg"] = "Cadastrado!";
+            return View(zoo);
+        }
+    }
+}
---
+++ 05-Fiap.Web.AspNet/05-Fiap.Web.AspNet/Models/Zoo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _05_Fiap.Web.AspNet.Models
+{
+    public class Zoo
+    {
+        public int Jaulas { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Lançamento")]
+        public DateTime DataAbertura { get; set; }
+        public string Nome { get; set; }        
+        public Tipo EntidadeAdministradora { get; set; }
+
+    }
+}
